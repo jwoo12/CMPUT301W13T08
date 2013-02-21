d3cf5ed2b3bc6a4da7db24fa51e8757f490eadf1
jaeseo1 <jaeseo1@ualberta.ca>
2013-02-21 15:53:00 -0700

    RecipeDetails updates...

---
+++ ProjectRecipeBook/src/com/example/projectrecipebook/LocalRecipeBook.java
@@ -0,0 +1,15 @@
+package com.example.projectrecipebook;
+
+import android.os.Bundle;
+
+public class LocalRecipeBook extends TitleBarOverride {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.local_recipe_book);
+
+		
+	}
+	
+}
---
+++ ProjectRecipeBook/src/com/example/projectrecipebook/RecipeDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.projectrecipebook;
+
+import android.os.Bundle;
+
+public class RecipeDetailsActivity extends TitleBarOverride {
+	
+	@Override
+	public void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.recipe_details);
+		
+	}
+
+}
