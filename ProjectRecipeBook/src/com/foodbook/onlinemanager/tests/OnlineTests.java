b82e34f168f8eb12ebaa3a90cf38be7c8e73c65e
Jasmine <jwoo@ualberta.ca>
2013-03-09 18:35:12 -0700

    Made the package for web service testing

---
+++ ProjectRecipeBook/src/com/foodbook/onlinemanager/tests/OnlineTests.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.foodbook.onlinemanager.tests;
+
+import junit.framework.TestCase;
+import com.foodbook.onlinemanager.*;
+import com.foodbook.foodbook.*;
+
+
+/**
+ * 
+ * J-unit testing for web server
+ * 
+ * @author Jaeseo Park (jaeseo1), Jasmine Woo (jwoo), Nhu Bui (nbui), Robert Janes (rjanes)
+ *
+ */
+public class OnlineTests extends TestCase {
+
+	/**
+	 * 
+	 * 
+	 * 
+	 * @param name
+	 */
+	public OnlineTests(String name) {
+		super(name);
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
