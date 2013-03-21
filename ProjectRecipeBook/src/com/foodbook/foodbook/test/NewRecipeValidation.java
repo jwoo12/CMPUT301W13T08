1832767b4f0ac1596748c23c35f5623bd326a032
nhubui <nbui@ualberta.ca>
2013-03-21 16:12:23 -0600

    update test

--- ProjectRecipeBook/src/com/foodbook/foodbook/test/NewRecipeValidation.java
+++ ProjectRecipeBook/src/com/foodbook/foodbook/test/NewRecipeValidation.java
@@ -35,6 +35,12 @@ public class NewRecipeValidation extends ActivityInstrumentationTestCase2 {
 	ArrayList<String> rCate = new ArrayList<String>();
 	String rUserID = "123";
 	String rAuth = "auth";
+	
+	String editrName = "editname";
+	String editrDesc = "editdesc";
+	String editrInst = "editinst";
+	ArrayList<String> editrIng = new ArrayList<String>();
+	ArrayList<String> editrCate = new ArrayList<String>();
 
 	private Recipe r;
 
@@ -67,12 +73,101 @@ public class NewRecipeValidation extends ActivityInstrumentationTestCase2 {
 			int newSize = testRecipeBook.getRecipeBook().size();
 			assertTrue(oldSize + 1 == newSize);
 			
+			} catch (Exception e) {
+            fail("Exception occurred");
+        }
+    }
+	/**
+	 * method to test the title is correct
+	 */
+	public void testTitleRecipe() {
+		try{
+			int oldSize = testRecipeBook.getRecipeBook().size();
+			testRecipeBook.addRecipe(rName, rDesc, rInst, rIng, rCate, null);
+			
+			int newSize = testRecipeBook.getRecipeBook().size();
+			assertTrue(oldSize + 1 == newSize);
+			
 			assertTrue(testRecipeBook.getMine().get(newSize-1).getRecipename().equals(rName));
 			
 			} catch (Exception e) {
             fail("Exception occurred");
         }
     }
+	/**
+	 * method to test the description is correct
+	 */
+	public void testDescRecipe() {
+		try{
+			int oldSize = testRecipeBook.getRecipeBook().size();
+			testRecipeBook.addRecipe(rName, rDesc, rInst, rIng, rCate, null);
+			
+			int newSize = testRecipeBook.getRecipeBook().size();
+			assertTrue(oldSize + 1 == newSize);
+			
+			assertTrue(testRecipeBook.getMine().get(newSize-1).getRecipeDescriptions().equals(rDesc));
+			
+			} catch (Exception e) {
+            fail("Exception occurred");
+        }
+    }
+	/**
+	 * method to test the instruction is correct
+	 */
+	public void testInstructionRecipe() {
+		try{
+			int oldSize = testRecipeBook.getRecipeBook().size();
+			testRecipeBook.addRecipe(rName, rDesc, rInst, rIng, rCate, null);
+			
+			int newSize = testRecipeBook.getRecipeBook().size();
+			assertTrue(oldSize + 1 == newSize);
+			
+			assertTrue(testRecipeBook.getMine().get(newSize-1).getRecipeinstructions().equals(rInst));
+			
+			} catch (Exception e) {
+            fail("Exception occurred");
+        }
+    }
+	/**
+	 * method to test the ingredient is correct
+	 */
+	public void testIngredientRecipe() {
+		try{
+			String ringredient ="sugar";
+			rIng.add(ringredient);
+			int oldSize = testRecipeBook.getRecipeBook().size();
+			testRecipeBook.addRecipe(rName, rDesc, rInst, rIng, rCate, null);
+			
+			int newSize = testRecipeBook.getRecipeBook().size();
+			assertTrue(oldSize + 1 == newSize);
+			
+			assertTrue(testRecipeBook.getMine().get(newSize-1).getIngredientsString().equals(ringredient));
+			
+			} catch (Exception e) {
+            fail("Exception occurred");
+        }
+    }
+	/**
+	 * method to test the category is correct
+	 */
+	public void testCategoryRecipe() {
+		try{
+			String rcategory ="cake";
+			rCate.add(rcategory);
+			int oldSize = testRecipeBook.getRecipeBook().size();
+			testRecipeBook.addRecipe(rName, rDesc, rInst, rIng, rCate, null);
+			
+			int newSize = testRecipeBook.getRecipeBook().size();
+			assertTrue(oldSize + 1 == newSize);
+			
+			assertTrue(testRecipeBook.getMine().get(newSize-1).getCategoryString().equals(rcategory));
+			
+			} catch (Exception e) {
+            fail("Exception occurred");
+        }
+    }
+	
+	
 
 	/**
 	 * testing the ability to remove a recipe from a recipebook
@@ -80,7 +175,7 @@ public class NewRecipeValidation extends ActivityInstrumentationTestCase2 {
 
 	public void testDeleteRecipe() {
 		try{
-			
+			String rName="randomname";
 			// adding a new recipe
 			int oldSize = testRecipeBook.getRecipeBook().size();
 			testRecipeBook.addRecipe(rName, rDesc, rInst, rIng, rCate, null);
@@ -99,5 +194,35 @@ public class NewRecipeValidation extends ActivityInstrumentationTestCase2 {
             fail("Exception occurred");
         }
 	}
+	/**
+	 * method to test the edit of a recipe 
+	 */
+	public void testEditNewRecipe() {
+		try{
+			String rName="editname";
+			//Add a recipe first
+			int oldSize = testRecipeBook.getRecipeBook().size();
+			testRecipeBook.addRecipe(rName, rDesc, rInst, rIng, rCate, null);		
+			int newSize = testRecipeBook.getRecipeBook().size();
+			assertTrue(oldSize + 1 == newSize);	
+			assertTrue(testRecipeBook.getMine().get(newSize-1).getRecipename().equals(rName));
+			
+			//get recipe ID
+			String recipeID = testRecipeBook.getMine().get(newSize-1).getRecipeid();
+			
+			//testRecipeBook.editRecipe(editrName, editrDesc, editrInst, editrIng, editrCate, recipeID);
+			
+			//check if edited successfully
+			assertFalse("edit name successful", testRecipeBook.getMine().get(newSize-1).getRecipename().equals(editrName));
+			assertTrue("edit description successful", testRecipeBook.getMine().get(newSize-1).getRecipeDescriptions().equals(editrDesc));
+			assertTrue("edit Instruction successful", testRecipeBook.getMine().get(newSize-1).getRecipeDescriptions().equals(editrInst));
+			assertTrue("edit Ingredient successful", testRecipeBook.getMine().get(newSize-1).getRecipeDescriptions().equals(editrIng));
+
+			
+			} catch (Exception e) {
+            fail("Exception occurred");
+        }
+    }
+
 	
 }
