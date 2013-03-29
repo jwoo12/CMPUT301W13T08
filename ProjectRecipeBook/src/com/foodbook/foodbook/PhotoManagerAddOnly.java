db1553c68a78a0ee4fb41a2f52cb519fab808681
jaeseo1 <jaeseo1@ualberta.ca>
2013-03-28 21:43:37 -0600

    removed libraries

---
+++ ProjectRecipeBook/src/com/foodbook/foodbook/PhotoManagerAddOnly.java
@@ -0,0 +1,13 @@
+package com.foodbook.foodbook;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class PhotoManagerAddOnly extends PhotoManager {
+
+	@Override
+	protected void deletePic(int position, Context applicationContext) {
+		Toast.makeText(this, "Not author: you can only view!", Toast.LENGTH_LONG).show();
+	}
+
+}
